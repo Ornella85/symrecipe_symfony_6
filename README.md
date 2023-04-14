25f79091ccfc3748dbd19e92258d2108d916c60f
ornella <ornella.lallier@cfnews.net>
2023-04-14 17:00:46 +0200

    CRUD :  knp_paginator / FormType

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ### TIPS
+
 bootswatch 
+
 gitmoji.dev
+
+### Bundle :
+composer require knplabs/knp-paginator-bundle
+
+composer require fakerphp/faker --dev
--- src/Controller/IngredientsController.php
+++ src/Controller/IngredientsController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Ingredients;
+use App\Repository\IngredientsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class IngredientsController extends AbstractController
 {
-    #[Route('/pages', name: 'ingredients.index')]
-    public function index(): Response
+    /**
+     * This function display all ingredients
+     * @param IngredientsRepository $ingredientsRepository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+
+    #[Route('/ingredients', name: 'ingredients.index')]
+    public function index(IngredientsRepository $ingredientsRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $ingredients = $paginator->paginate(
+            $ingredientsRepository->findAll (),
+            $request->query->getInt('page', 1),
+            10
+        );
         return $this->render('pages/ingredients/index.html.twig', [
-            'controller_name' => 'IngredientsController',
+            'ingredients' => $ingredients,
         ]);
     }
 }
