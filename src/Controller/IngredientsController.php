d67d15053e367a69b98e042d2038a7b8b66d0df1
ornella <ornella.lallier@cfnews.net>
2023-04-14 17:41:21 +0200

    CRUD :  create / FormType 🔨

--- src/Controller/IngredientsController.php
+++ src/Controller/IngredientsController.php
@@ -3,14 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Ingredients;
+use App\Form\IngredientsFormType;
 use App\Repository\IngredientsRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -24,7 +23,7 @@ class IngredientsController extends AbstractController
      * @return Response
      */
 
-    #[Route('/ingredients', name: 'ingredients.index')]
+    #[Route('/ingredients', name: 'ingredients.index', methods:['GET'])]
     public function index(IngredientsRepository $ingredientsRepository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
@@ -36,4 +35,32 @@ class IngredientsController extends AbstractController
             'ingredients' => $ingredients,
         ]);
     }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/ingredients/new', name: 'ingredients.new', methods:['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $ingredient = new Ingredients();
+        $form = $this->createForm(IngredientsFormType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash('success', 'Votre ingrédient a été créé avec succès !');
+
+            return $this->redirectToRoute('ingredients.index');
+        }
+
+        return $this->render('pages/ingredients/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
 }
--- src/Entity/Ingredients.php
+++ src/Entity/Ingredients.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\IngredientsRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: IngredientsRepository::class)]
+#[UniqueEntity('name')]
 class Ingredients
 {
     #[ORM\Id]
