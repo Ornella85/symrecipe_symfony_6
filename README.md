32f90b2f1a8cfb2e549014f3a2b24256a32c2559
ornella <ornella.lallier@cfnews.net>
2023-06-01 15:58:32 +0200

    symRecipe : Recaptcha

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 ### TIPS
 
-bootswatch 
+bootswatch
 
 gitmoji.dev
 
-### Bundle :
+### Bundles :
 composer require knplabs/knp-paginator-bundle
 
 composer require fakerphp/faker --dev
 
+composer require karser/karser-recaptcha3-bundle
 
 ### ligne de commande
-php bin/console doctrine:fixtures:load 
+
+#### Faker - générer les fixtures : php bin/console doctrine:fixtures:load 
  
+
+### Liens
+#### Goggle Recaptcha : https://developers.google.com/recaptcha/docs/v3?hl=fr
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Contact;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -78,6 +80,10 @@ class ContactType extends AbstractType
                     'class' => 'btn btn-primary mt-4'
                 ],
                 'label' => 'Envoyer ma demande ',
+            ])
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
             ])
         ;
     }
